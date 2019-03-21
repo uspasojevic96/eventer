f9634a7faf8ef17b2328cce9e8ab573d9d1152f1
Uros Spasojevic <uspasojevic96@gmail.com>
2019-03-21 22:21:01 +0100

    Added docs

--- src/EventManager.ts
+++ src/EventManager.ts
@@ -1,17 +1,24 @@
+/**
+ * Eventer EventManager class
+ * 
+ * @author Uros Spasojevic
+ */
 export class EventManager {
+    /**
+     * Object containing all active events and their corresponding listeners
+     * 
+     * @type {Object}
+     * @private
+     */
     private events: any = {};
 
-    public on(event: string, callback: Function): void {
-        let events = this.events[event];
-        if(!events) {
-            events = [];
-        }
-
-        events.push(callback);
-
-        this.events[event] = events;
-    }
-
+    /**
+     * Fires the specified event with optional passed data
+     * 
+     * @param {string} event Event name 
+     * @param {any} data Optional data for specified event
+     * @public
+     */
     public fireEvent(event: string, data?: any): void {
         let events = this .getEventListeners(event);
         if(events) {
@@ -24,10 +31,41 @@ export class EventManager {
         }
     }
 
+    /**
+     * Returns listeners for specified event
+     * 
+     * @param {string} event Event name
+     * @returns {Function[]}
+     * @private
+     */
     private getEventListeners(event: string): Function[] {
         return this.events[event];
     }
 
+    /**
+     * Subscribes to specified event with passed callback
+     * 
+     * @param {string} event Event name 
+     * @param {Function} callback Handler callback 
+     */
+    public on(event: string, callback: Function): void {
+        let events = this.events[event];
+
+        if(!events) {
+            events = [];
+        }
+
+        events.push(callback);
+
+        this.events[event] = events;
+    }
+
+    /**
+     * Unsubscribes from specified event with passed callback
+     * 
+     * @param {string} event 
+     * @param {Function} callback 
+     */
     public un(event: string, callback: Function): void {
         let events = this.getEventListeners(event);
 
